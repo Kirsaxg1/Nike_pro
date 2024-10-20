6590424ba39d87f90f324c49d3d31bf3c830604b
Kirsaxg1 <dooe_x@vk.com>
2024-10-20 21:56:39 +0300

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,26 +1,28 @@
 //#include <stdio.h>
+//#include <stdlib.h>
+//#define XMAX 110
+//#define XMIN 1
 //#define SIZE 10
-//
 //int main() {
 //
-//	int arr[SIZE], newarr[ SIZE / 2 ], i, j;
+//	int arr[SIZE], newarr[SIZE / 2], i, j;
 //	j = 0;
 //
-//	printf("Enter a arr ( 10 elements ): \n");
-//	for (int i = 0; i < SIZE; i++) {
-//		scanf("%d", &arr[i]);
-//	}
-//
-//	printf("The entered array:\n ");
 //	for (i = 0; i < SIZE; i++) {
-//		printf("% d,", arr[i]);
+//		arr[i] = XMIN + 1 * (XMAX - XMIN) * rand() / RAND_MAX;
+//		printf("\na[%d] = %d\n", i + 1, arr[i]);
 //	}
 //
-//	printf("\nAn array of numbers in odd positions: \n");
-//	for (i = 1; i < SIZE; i += 2) {
-//		newarr[j] = arr[i];
-//		j++;
-//		printf("% d,", newarr[j - 1]);
+//		printf("The entered array:\n ");
+//		for (i = 0; i < SIZE; i++) {
+//			printf("% d,", arr[i]);
+//		}
+//	
+//		printf("\nAn array of numbers in odd positions: \n");
+//		for (i = 1; i < SIZE; i += 2) {
+//			newarr[j] = arr[i];
+//			j++;
+//			printf("% d,", newarr[j - 1]);
+//		}
+//		return 0;
 //	}
-//	return 0;
-//}
